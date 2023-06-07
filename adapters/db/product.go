09eae9d74af1bf3a88349d0de78f122575af1528
Lucas Madeira <lmadeira.ec@gmail.com>
2023-06-07 14:12:07 -0400

    first commmit

---
+++ adapters/db/product.go
@@ -0,0 +1,86 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/codeedu/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDB struct {
+	db *sql.DB
+}
+
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (p *ProductDB) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("SELECT id, name, price, status from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (p *ProductDB) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("INSERT INTO products(id,name,price,status) VALUES (?,?,?,?)")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus())
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+
+}
+
+func (p *ProductDB) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("UPDATE products SET name = ?, price = ?, status = ? WHERE id = ?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDB) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("SELECT id FROM products WHERE id = ?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		_, err := p.create(product)
+
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return product, nil
+}
---
+++ adapters/db/product_test.go
@@ -0,0 +1,87 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/adapters/db"
+	"github.com/codeedu/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products(
+		"id" string,
+		"name" string,
+		"price" float,
+		"status" string
+	)`
+
+	stmt, err := db.Prepare(table)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products VALUES("abc", "Product Test", 0, "disabled")`
+
+	stmt, err := db.Prepare(insert)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func TestProductDB_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDB(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+
+}
+
+func TestProductDB_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDB(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.GetName(), productResult.GetName())
+	require.Equal(t, product.GetPrice(), productResult.GetPrice())
+	require.Equal(t, product.GetStatus(), productResult.GetStatus())
+
+	product.Enable()
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.GetName(), productResult.GetName())
+	require.Equal(t, product.GetPrice(), productResult.GetPrice())
+	require.Equal(t, product.GetStatus(), productResult.GetStatus())
+
+}
